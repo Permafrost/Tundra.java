d96712150601ff037cdc78a666a6f611bd3f5014
Lachlan Dowding <lachlan@permafro.st>
2015-07-03 11:46:25 +1000

    Refactor NameHelper domain name and localhost resolution

--- src/main/java/permafrost/tundra/server/NameHelper.java
+++ src/main/java/permafrost/tundra/server/NameHelper.java
@@ -25,7 +25,7 @@
 package permafrost.tundra.server;
 
 import com.wm.data.IData;
-import permafrost.tundra.data.IDataHelper;
+import com.wm.util.coder.IDataCodable;
 import permafrost.tundra.data.IDataMap;
 import permafrost.tundra.lang.BaseException;
 import permafrost.tundra.lang.ExceptionHelper;
@@ -34,10 +34,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class NameHelper {
-    private static final long LOCALHOST_CACHE_DURATION = 5 * 60 * 1000; // 5 minutes
-    private static volatile long localhostCacheExpiry = 0;
-    private static volatile IData localhost = null;
-
     /**
      * Disallow instantiation of this class.
      */
@@ -55,7 +51,7 @@ public class NameHelper {
     public static IData resolve(String name) throws BaseException {
         IData output = null;
         try {
-            output = toIData(InetAddress.getByName(name));
+            output = InternetAddress.resolve(name).getIData();
         } catch (UnknownHostException ex) {
             ExceptionHelper.raise(ex);
         }
@@ -72,11 +68,7 @@ public class NameHelper {
     public static IData localhost() throws BaseException {
         IData output = null;
         try {
-            if (localhost == null || localhostCacheExpiry <= System.currentTimeMillis()) {
-                localhost = toIData(InetAddress.getLocalHost());
-                localhostCacheExpiry = System.currentTimeMillis() + LOCALHOST_CACHE_DURATION;
-            }
-            output = IDataHelper.duplicate(localhost);
+            output = InternetAddress.localhost().getIData();
         } catch(UnknownHostException ex) {
             ExceptionHelper.raise(ex);
         }
@@ -84,18 +76,98 @@ public class NameHelper {
     }
 
     /**
-     * Returns an IData representation of the given internet address.
-     * @param address The address to be converted to an IDataMap.
-     * @return        An IData representation of the given address,
-     *                containing the following keys: $domain, $host, $ip.
+     * Convenience class for resolving domain names and converting to IData representations.
      */
-    private static IData toIData(InetAddress address) {
-        if (address == null) return null;
+    private static class InternetAddress implements IDataCodable {
+        /**
+         * Cache localhost in volatile class member to optimize performance by avoiding the thread synchronization
+         * in the java.net.InetAddress class.
+         */
+        protected static final long LOCALHOST_EXPIRE_DURATION = 5 * 60 * 1000; // 5 minutes
+        protected static volatile long localhostExpireTime = 0;
+        protected static volatile InternetAddress localhost;
 
-        IDataMap output = new IDataMap();
-        output.put("$domain", address.getCanonicalHostName().toLowerCase());
-        output.put("$host", address.getHostName().toLowerCase());
-        output.put("$ip", address.getHostAddress().toLowerCase());
-        return output;
+        protected String domain, host, ip;
+
+        /**
+         * Create a new InternetAddress from the given InetAddress object.
+         * @param address The address to use to create this object.
+         */
+        public InternetAddress(InetAddress address) {
+            if (address == null) throw new NullPointerException("address must not be null");
+            this.domain = address.getCanonicalHostName().toLowerCase();
+            this.host = address.getHostName().toLowerCase();
+            this.ip = address.getHostAddress().toLowerCase();
+        }
+
+        /**
+         * Returns the domain name associated with this internet address.
+         * @return The domain name associated with this internet address.
+         */
+        public String getDomain() {
+            return domain;
+        }
+
+        /**
+         * Returns the host name associated with this internet address.
+         * @return The host name associated with this internet address.
+         */
+        public String getHost() {
+            return host;
+        }
+
+        /**
+         * Returns the IP address associated with this internet address.
+         * @return The IP address associated with this internet address.
+         */
+        public String getIPAddress() {
+            return ip;
+        }
+
+        /**
+         * Returns an IData representation of this internet address.
+         * @return        An IData representation of this internet address,
+         *                containing the following keys: $domain, $host, $ip.
+         */
+        public IData getIData() {
+            IDataMap output = new IDataMap();
+            output.put("$domain", domain);
+            output.put("$host", host);
+            output.put("$ip", ip);
+            return output;
+        }
+
+        /**
+         * This method is not implemented by this class.
+         * @param input An IData to use to set the member variables of this class.
+         * @throws UnsupportedOperationException Because this method is not implemented by this class.
+         */
+        public void setIData(IData input) {
+            throw new UnsupportedOperationException("setIData is not supported by this class");
+        }
+
+        /**
+         * Returns the localhost address.
+         * @return The localhost address.
+         * @throws UnknownHostException If the localhost cannot be resolved.
+         */
+        public static InternetAddress localhost() throws UnknownHostException {
+            // thread synchronization is not required because we don't care if we miss an update
+            if (localhost == null || localhostExpireTime <= System.currentTimeMillis()) {
+                localhost = new InternetAddress(InetAddress.getLocalHost());
+                localhostExpireTime = System.currentTimeMillis() + LOCALHOST_EXPIRE_DURATION;
+            }
+            return localhost;
+        }
+
+        /**
+         * Returns the internet address that the given name resolves to.
+         * @param name  The name to be resolved.
+         * @return      The internet address which resolves to the given name.
+         * @throws UnknownHostException If the name cannot be resolved.
+         */
+        public static InternetAddress resolve(String name) throws UnknownHostException {
+            return new InternetAddress(InetAddress.getByName(name));
+        }
     }
 }
