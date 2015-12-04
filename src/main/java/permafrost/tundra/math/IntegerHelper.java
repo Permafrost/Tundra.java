da88d9aa895c4c283abf2a7e014ae290e4439a31
Lachlan Dowding <lachlan@permafro.st>
2015-12-05 08:22:51 +1000

    Add IntegerHelper.parse and .emit methods for arrays

--- src/main/java/permafrost/tundra/math/IntegerHelper.java
+++ src/main/java/permafrost/tundra/math/IntegerHelper.java
@@ -29,6 +29,11 @@ package permafrost.tundra.math;
  */
 public class IntegerHelper {
     /**
+     * The default value used when parsing a null string.
+     */
+    private static int DEFAULT_INT_VALUE = 0;
+
+    /**
      * Disallow instantiation of this class.
      */
     private IntegerHelper() {}
@@ -40,7 +45,7 @@ public class IntegerHelper {
      * @return      Integer representing the given string, or 0 if the given string was null.
      */
     public static int parse(String input) {
-        return parse(input, 0);
+        return parse(input, DEFAULT_INT_VALUE);
     }
 
     /**
@@ -53,5 +58,60 @@ public class IntegerHelper {
     public static int parse(String input, int defaultValue) {
         if (input == null) return defaultValue;
         return Integer.parseInt(input);
+    }
+
+    /**
+     * Parses the given strings as integers.
+     *
+     * @param input         A list of strings to be parsed as integers.
+     * @return              A list of integers representing the given strings.
+     */
+    public static int[] parse(String[] input) {
+        return parse(input, DEFAULT_INT_VALUE);
+    }
+
+    /**
+     * Parses the given strings as integers.
+     *
+     * @param input         A list of strings to be parsed as integers.
+     * @param defaultValue  The value returned if a string in the list is null.
+     * @return              A list of integers representing the given strings.
+     */
+    public static int[] parse(String[] input, int defaultValue) {
+        if (input == null) return null;
+        int[] output = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            output[i] = parse(input[i], defaultValue);
+        }
+
+        return output;
+    }
+
+    /**
+     * Serializes the given integer as a string.
+     *
+     * @param input The integer to be serialized.
+     * @return      A string representation of the given integer.
+     */
+    public static String emit(int input) {
+        return Integer.toString(input);
+    }
+
+    /**
+     * Serializes the given integers as strings.
+     *
+     * @param input A list of integers to be serialized.
+     * @return      A list of string representations of the given integers.
+     */
+    public static String[] emit(int[] input) {
+        if (input == null) return null;
+        String[] output = new String[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            output[i] = emit(input[i]);
+        }
+
+        return output;
     }
 }
