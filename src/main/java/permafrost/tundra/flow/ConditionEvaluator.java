b864e4061f34459da6b9b686ccd4f04a30ff5e5d
Lachlan Dowding <lachlan@permafro.st>
2016-05-31 13:48:09 +1000

    Remove support for true/false/null key words from ConditionEvaluator

--- src/main/java/permafrost/tundra/flow/ConditionEvaluator.java
+++ src/main/java/permafrost/tundra/flow/ConditionEvaluator.java
@@ -25,26 +25,14 @@
 package permafrost.tundra.flow;
 
 import com.wm.data.IData;
-import com.wm.data.IDataCursor;
 import com.wm.data.IDataFactory;
-import com.wm.data.IDataUtil;
 import com.wm.lang.flow.ExpressionEvaluator;
 import com.wm.lang.flow.MalformedExpressionException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Performs webMethods Integration Server flow language conditional statement evaluation against a specified scope.
  */
 public class ConditionEvaluator {
-    /**
-     * Regular expressions to detect backwards-compatibility mode to support the previous ANTLR-based implementation of
-     * the evaluate function, which allowed use of the key words: null, true and false
-     */
-    protected static final Pattern NULL_PATTERN = Pattern.compile("((=|==|!=|<>|>|>=|<|<=)\\s*null(\\s|$))|((^|\\s)null\\s*(=|==|!=|<>|>|>=|<|<=))");
-    protected static final Pattern TRUE_PATTERN = Pattern.compile("((=|==|!=|<>|>|>=|<|<=)\\s*true(\\s|$))|((^|\\s)true\\s*(=|==|!=|<>|>|>=|<|<=))");
-    protected static final Pattern FALSE_PATTERN = Pattern.compile("((=|==|!=|<>|>|>=|<|<=)\\s*false(\\s|$))|((^|\\s)false\\s*(=|==|!=|<>|>|>=|<|<=))");
-
     /**
      * The conditional statement to be evaluated by this object.
      */
@@ -69,32 +57,8 @@ public class ConditionEvaluator {
         boolean result = true;
 
         if (condition != null) {
-            Matcher nullMatcher = NULL_PATTERN.matcher(condition);
-            Matcher trueMatcher = TRUE_PATTERN.matcher(condition);
-            Matcher falseMatcher = FALSE_PATTERN.matcher(condition);
-
-            boolean nullFound = nullMatcher.find();
-            boolean trueFound = trueMatcher.find();
-            boolean falseFound = falseMatcher.find();
-
-            boolean backwardsCompatibilityRequired = nullFound || trueFound || falseFound;
-
-            if (scope == null) {
-                scope = IDataFactory.create();
-            } else {
-                if (backwardsCompatibilityRequired) scope = IDataUtil.clone(scope);
-            }
-
-            if (backwardsCompatibilityRequired) {
-                IDataCursor cursor = scope.getCursor();
-                if (nullFound) IDataUtil.put(cursor, "null", null);
-                if (trueFound) IDataUtil.put(cursor, "true", "true");
-                if (falseFound) IDataUtil.put(cursor, "false", "false");
-                cursor.destroy();
-            }
-
             try {
-                result = ExpressionEvaluator.evalToBoolean(condition, scope);
+                result = ExpressionEvaluator.evalToBoolean(condition, scope == null ? IDataFactory.create() : scope);
             } catch (MalformedExpressionException ex) {
                 throw new IllegalArgumentException(ex);
             }
