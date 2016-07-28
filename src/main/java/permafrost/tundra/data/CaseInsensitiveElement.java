c913a93a1a7aa5d26a14a6a5cd62c44c149bdf9b
Lachlan Dowding <lachlan@permafro.st>
2016-07-28 13:27:22 +1000

    Refactor CaseInsensitiveElementList and CaseInsensitiveElement classes

---
+++ src/main/java/permafrost/tundra/data/CaseInsensitiveElement.java
@@ -0,0 +1,226 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2016 Lachlan Dowding
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package permafrost.tundra.data;
+
+import permafrost.tundra.lang.CaseInsensitiveString;
+import permafrost.tundra.lang.LocaleHelper;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * An element whose keys are case-insensitive for comparison and case-preserving for reference.
+ *
+ * @param <V> The value's class.
+ */
+public class CaseInsensitiveElement<V> extends KeyAliasElement<String, V> implements Serializable {
+    /**
+     * The serialization class version identity.
+     */
+    private static final long serialVersionUID = 1;
+    /**
+     * Case insensitive version of the key.
+     */
+    protected CaseInsensitiveString caseInsensitiveKey;
+    /**
+     * The locale to use for case insensitivity.
+     */
+    protected Locale locale;
+
+    /**
+     * Constructs a new element using the given existing element.
+     *
+     * @param element   The element to use when constructing this new element.
+     */
+    public CaseInsensitiveElement(Element<String, V> element) {
+        this(element, null);
+    }
+
+    /**
+     * Constructs a new element using the given key and value.
+     *
+     * @param element   The element to use when constructing this new element.
+     * @param locale    The locale to use for case comparisons.
+     */
+    public CaseInsensitiveElement(Element<String, V> element, Locale locale) {
+        this(element.getKey(), element.getValue(), locale);
+    }
+
+    /**
+     * Constructs a new element using the given key and value.
+     *
+     * @param key       The key for the element.
+     * @param value     The value for the element.
+     */
+    public CaseInsensitiveElement(String key, V value) {
+        this(key, value, null);
+    }
+
+    /**
+     * Constructs a new element using the given key and value.
+     *
+     * @param key       The key for the element.
+     * @param value     The value for the element.
+     * @param locale    The locale used for case comparison.
+     */
+    public CaseInsensitiveElement(String key, V value, Locale locale) {
+        this(key, value, locale, (Collection<String>)null);
+    }
+
+    /**
+     * Constructs a new element using the given key and value.
+     *
+     * @param key       The key for the element.
+     * @param value     The value for the element.
+     * @param locale    The locale used for case comparison.
+     */
+    public CaseInsensitiveElement(String key, V value, Locale locale, String ...aliases) {
+        this(key, value, locale, aliases == null ? null : Arrays.asList(aliases));
+    }
+
+    /**
+     * Constructs a new element using the given key and value.
+     *
+     * @param key       The key for the element.
+     * @param value     The value for the element.
+     * @param locale    The locale used for case comparison.
+     */
+    public CaseInsensitiveElement(String key, V value, Locale locale, Collection<String> aliases) {
+        super(key, value, aliases);
+        setLocale(locale);
+        setKey(key);
+    }
+
+    /**
+     * Returns the key of the element.
+     *
+     * @return The key of the element.
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the key of the element.
+     *
+     * @param newKey    The key to be set.
+     * @return          The previous key of the element.
+     */
+    public String setKey(String newKey) {
+        if (newKey == null) throw new NullPointerException("key must not be null");
+        String oldKey = key;
+        key = newKey;
+        caseInsensitiveKey = new CaseInsensitiveString(key, locale);
+        return oldKey;
+    }
+
+    /**
+     * Returns the locale associated with this element.
+     *
+     * @return The locale associated with this element.
+     */
+    public Locale getLocale() {
+        return locale;
+    }
+
+    /**
+     * Sets the locale to use for case comparison.
+     *
+     * @param locale The locale to use for case comparison.
+     */
+    private void setLocale(Locale locale) {
+        this.locale = LocaleHelper.normalize(locale);
+    }
+
+    /**
+     * Returns true if another entry's key is equal to this entry's key.
+     *
+     * @param other The other entry to compare key equality to.
+     * @return      True if the other entry's key is equal to this entry's key.
+     */
+    public boolean keyEquals(String other) {
+        boolean result = caseInsensitiveKey.equals(other);
+        if (result) return true;
+
+        for (String alias : keyAliases) {
+            result = new CaseInsensitiveString(alias, locale).equals(other);
+            if (result) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Compares this element's key with the given other element's key, if they implement the Comparable interface.
+     *
+     * @param other The other element to compare this object with.
+     * @return      The comparison result.
+     */
+    @SuppressWarnings("unchecked")
+    public int compareTo(Map.Entry<? extends String, ? extends V> other) {
+        return caseInsensitiveKey.compareTo(other.getKey());
+    }
+
+    /**
+     * Returns the hash code for this element.
+     *
+     * @return The hash code for this element.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public int hashCode() {
+        return getKey().hashCode();
+    }
+
+    /**
+     * Returns the given element if it is already a CaseInsensitiveElement that uses the specified
+     * locale, otherwise returns a new CaseInsensitiveElement with the given element's key and value.
+     *
+     * @param element               The element to be normalized.
+     * @param locale                Optional locale to use for case comparison.
+     * @param <V>                   The class of values held by the element.
+     * @return                      The normalized element.
+     * @throws NullPointerException If the given element is null.
+     */
+    @SuppressWarnings("unchecked")
+    public static <V> Element<String, V> normalize(Element<String, V> element, Locale locale) {
+        CaseInsensitiveElement<V> output = null;
+
+        if (element instanceof CaseInsensitiveElement) {
+            locale = LocaleHelper.normalize(locale);
+            if (!locale.equals(((CaseInsensitiveElement)element).getLocale())) {
+                output = new CaseInsensitiveElement<V>((CaseInsensitiveElement)element, locale);
+            } else {
+                output = (CaseInsensitiveElement)element;
+            }
+        } else {
+            output = new CaseInsensitiveElement<V>(element, locale);
+        }
+
+        return output;
+    }
+}
--- src/main/java/permafrost/tundra/data/CaseInsensitiveElementList.java
+++ src/main/java/permafrost/tundra/data/CaseInsensitiveElementList.java
@@ -113,13 +113,22 @@ public class CaseInsensitiveElementList<V> extends ElementList<String, V> implem
     }
 
     /**
-     * Recursively builds a clone of the given IData document as a new CaseInsensitiveElementList.
+     * Builds a clone of the given Map as a new CaseInsensitiveElementList.
      *
-     * @param document  The document to clone.
+     * @param map       The map to clone.
+     * @param locale    The locale used for case comparison.
      * @return          A new CaseInsensitiveElementList which is a recursive clone of the given IData document.
      */
-    public static IData of(IData document) {
-        return of(document, null);
+    public static IData of(Map map, Locale locale) {
+        CaseInsensitiveElementList<Object> output = new CaseInsensitiveElementList<Object>();
+
+        if (map != null) {
+            for (Object key : map.keySet()) {
+                output.add(new CaseInsensitiveElement<Object>(key.toString(), map.get(key), locale));
+            }
+        }
+
+        return output;
     }
 
     /**
@@ -141,16 +150,6 @@ public class CaseInsensitiveElementList<V> extends ElementList<String, V> implem
         }
 
         return output;
-    }
-
-    /**
-     * Recursively builds a clone of the given IData[] document list as a new CaseInsensitiveElementList[].
-     *
-     * @param documents The documents to clone.
-     * @return          A new CaseInsensitiveElementList[] which is a recursive clone of the given IData[] documents.
-     */
-    public static IData[] of(IData[] documents) {
-        return of(documents, null);
     }
 
     /**
@@ -191,320 +190,6 @@ public class CaseInsensitiveElementList<V> extends ElementList<String, V> implem
             value = (IData)of(IDataHelper.toIData(value), locale);
         }
         return value;
-    }
-
-    /**
-     * An element whose keys are case-insensitive for comparison and case-preserving for reference.
-     *
-     * @param <V> The value's class.
-     */
-    protected static class CaseInsensitiveElement<V> extends Element<String, V> {
-        /**
-         * Case insensitive version of the key.
-         */
-        protected CaseInsensitiveString caseInsensitiveKey;
-        /**
-         * The locale to use for case insensitivity.
-         */
-        protected Locale locale;
-
-        /**
-         * Constructs a new element using the given existing element.
-         *
-         * @param element   The element to use when constructing this new element.
-         */
-        public CaseInsensitiveElement(Element<String, V> element) {
-            this(element, null);
-        }
-
-        /**
-         * Constructs a new element using the given key and value.
-         *
-         * @param element   The element to use when constructing this new element.
-         * @param locale    The locale to use for case comparisons.
-         */
-        public CaseInsensitiveElement(Element<String, V> element, Locale locale) {
-            this(element.getKey(), element.getValue(), locale);
-        }
-
-        /**
-         * Constructs a new element using the given key and value.
-         *
-         * @param key       The key for the element.
-         * @param value     The value for the element.
-         */
-        public CaseInsensitiveElement(String key, V value) {
-            this(key, value, null);
-        }
-
-        /**
-         * Constructs a new element using the given key and value.
-         *
-         * @param key       The key for the element.
-         * @param value     The value for the element.
-         * @param locale    The locale used for case comparison.
-         */
-        public CaseInsensitiveElement(String key, V value, Locale locale) {
-            setLocale(locale);
-            setKey(key);
-            setValue(value);
-        }
-
-        /**
-         * Returns the key of the element.
-         *
-         * @return The key of the element.
-         */
-        public String getKey() {
-            return key;
-        }
-
-        /**
-         * Sets the key of the element.
-         *
-         * @param newKey    The key to be set.
-         * @return          The previous key of the element.
-         */
-        public String setKey(String newKey) {
-            if (newKey == null) throw new NullPointerException("key must not be null");
-            String oldKey = key;
-            key = newKey;
-            caseInsensitiveKey = new CaseInsensitiveString(key, locale);
-            return oldKey;
-        }
-
-        /**
-         * Returns the locale associated with this element.
-         *
-         * @return The locale associated with this element.
-         */
-        public Locale getLocale() {
-            return locale;
-        }
-
-        /**
-         * Sets the locale to use for case comparison.
-         *
-         * @param locale The locale to use for case comparison.
-         */
-        private void setLocale(Locale locale) {
-            this.locale = LocaleHelper.normalize(locale);
-        }
-
-        /**
-         * Returns whether this element is equivalent to the given other element.
-         *
-         * @param other The other element to compare equivalency with.
-         * @return      True if this element is equivalent to the give other element.
-         */
-        @Override
-        @SuppressWarnings("unchecked")
-        public boolean equals(Object other) {
-            if (this == other) return true;
-
-            boolean result = false;
-            if (other instanceof Map.Entry) {
-                result = keyEquals((Map.Entry)other);
-            }
-
-            return result;
-        }
-
-        /**
-         * Returns true if another entry's key is equal to this entry's key.
-         *
-         * @param other The other entry to compare key equality to.
-         * @return      True if the other entry's key is equal to this entry's key.
-         */
-        @Override
-        public boolean keyEquals(String other) {
-            return caseInsensitiveKey.equals(other);
-        }
-
-        /**
-         * Compares this element's key with the given other element's key, if they implement the Comparable interface.
-         *
-         * @param other The other element to compare this object with.
-         * @return      The comparison result.
-         */
-        @SuppressWarnings("unchecked")
-        public int compareTo(Map.Entry<? extends String, ? extends V> other) {
-            return caseInsensitiveKey.compareTo(other.getKey());
-        }
-
-        /**
-         * Returns the hash code for this element.
-         *
-         * @return The hash code for this element.
-         */
-        @Override
-        @SuppressWarnings("unchecked")
-        public int hashCode() {
-            return getKey().hashCode();
-        }
-
-        /**
-         * Returns a new CaseInsensitiveElement using the given element's key and value.
-         *
-         * @param element               An element to be represented as a CaseInsensitiveElement.
-         * @param locale                The locale to use for case comparison.
-         * @param <V>                   The class of values held by the element.
-         * @return                      A new CaseInsensitiveElement with the given element's key and value.
-         * @throws NullPointerException If the given element is null.
-         */
-        public static <V> CaseInsensitiveElement<V> of(Element<String, V> element, Locale locale) {
-            return new CaseInsensitiveElement<V>(element, locale);
-        }
-
-        /**
-         * Returns the given element if it is already a CaseInsensitiveElement that uses the specified
-         * locale, otherwise returns a new CaseInsensitiveElement with the given element's key and value.
-         *
-         * @param element               The element to be normalized.
-         * @param <V>                   The class of values held by the element.
-         * @return                      The normalized element.
-         * @throws NullPointerException If the given element is null.
-         */
-        public static <V> CaseInsensitiveElement<V> normalize(Element<String, V> element) {
-            return normalize(element, null);
-        }
-
-        /**
-         * Returns the given element if it is already a CaseInsensitiveElement that uses the specified
-         * locale, otherwise returns a new CaseInsensitiveElement with the given element's key and value.
-         *
-         * @param element               The element to be normalized.
-         * @param locale                The locale to use for case comparison.
-         * @param <V>                   The class of values held by the element.
-         * @return                      The normalized element.
-         * @throws NullPointerException If the given element is null.
-         */
-        @SuppressWarnings("unchecked")
-        public static <V> CaseInsensitiveElement<V> normalize(Element<String, V> element, Locale locale) {
-            CaseInsensitiveElement<V> output = null;
-
-            if (element instanceof CaseInsensitiveElement) {
-                locale = LocaleHelper.normalize(locale);
-                if (!locale.equals(((CaseInsensitiveElement)element).getLocale())) {
-                    output = new CaseInsensitiveElement<V>((CaseInsensitiveElement)element, locale);
-                } else {
-                    output = (CaseInsensitiveElement)element;
-                }
-            } else {
-                output = of(element, locale);
-            }
-
-            return output;
-        }
-    }
-
-    /**
-     * A case insensitive for comparison and case preserving for reference string wrapper.
-     */
-    protected static class CaseInsensitiveString implements CharSequence, Comparable<CharSequence>, Serializable {
-        /**
-         * The serialization identity for this class and version.
-         */
-        private static final long serialVersionUID = 1;
-        /**
-         * The strings to be wrapped.
-         */
-        protected String originalString, lowercaseString;
-        /**
-         * The locale to use for case comparison.
-         */
-        protected Locale locale;
-
-        /**
-         * Constructs a new CaseInsensitiveString.
-         *
-         * @param charSequence The string to be wrapped.
-         */
-        public CaseInsensitiveString(CharSequence charSequence) {
-            this(charSequence, null);
-        }
-
-        /**
-         * Constructs a new CaseInsensitiveString.
-         *
-         * @param charSequence The string to be wrapped.
-         * @param locale       The locale to use for case comparison.
-         */
-        public CaseInsensitiveString(CharSequence charSequence, Locale locale) {
-            if (charSequence == null) throw new NullPointerException("charSequence must not be null");
-
-            this.locale = locale == null ? Locale.getDefault() : locale;
-            this.originalString = charSequence.toString();
-            this.lowercaseString = this.originalString.toLowerCase(this.locale);
-        }
-
-        /**
-         * Returns the character at the given index.
-         *
-         * @param i The character index.
-         * @return  The character at the given index.
-         */
-        public char charAt(int i) {
-            return this.originalString.charAt(i);
-        }
-
-        /**
-         * Returns the number of characters in this string.
-         *
-         * @return The number of characters in this string.
-         */
-        public int length() {
-            return this.originalString.length();
-        }
-
-        /**
-         * Returns a sub-sequence for the given range of characters.
-         *
-         * @param start The start index for the range.
-         * @param end   The end index for the range.
-         * @return      The sub-sequence of characters for the given range.
-         */
-        public CaseInsensitiveString subSequence(int start, int end) {
-            return new CaseInsensitiveString(originalString.subSequence(start, end));
-        }
-
-        /**
-         * Performs a case-insensitive comparison with the other string.
-         *
-         * @param other A string to compared with this object.
-         * @return      The result of the comparison.
-         */
-        public int compareTo(CharSequence other) {
-            return this.lowercaseString.compareTo(other.toString().toLowerCase(locale));
-        }
-
-        /**
-         * Compares this object against another for equality.
-         *
-         * @param other The other object to compare this object against for equality.
-         * @return      True if the two objects are equal.
-         */
-        @Override
-        public boolean equals(Object other) {
-            if (this == other) return true;
-
-            boolean result = false;
-            if (other instanceof CharSequence) {
-                String otherString = ((CharSequence)other).toString();
-                result = this.lowercaseString.equals(otherString.toLowerCase(locale));
-            }
-            return result;
-        }
-
-        /**
-         * Returns the original string wrapped by this case-insensitive string object.
-         *
-         * @return The original string wrapped by this case-insensitive string object.
-         */
-        @Override
-        public String toString() {
-            return this.originalString;
-        }
     }
 
     /**
