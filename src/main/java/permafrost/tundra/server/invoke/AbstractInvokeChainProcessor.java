2dac8e7ba1ca85e404145e410590ff430a83a33f
Lachlan Dowding <lachlan@permafro.st>
2016-09-07 13:32:06 +1000

    Add AbstractInvokeChainProcessor class to refactor common code into this superclass

---
+++ src/main/java/permafrost/tundra/server/invoke/AbstractInvokeChainProcessor.java
@@ -0,0 +1,53 @@
+package permafrost.tundra.server.invoke;
+
+import com.wm.app.b2b.server.BaseService;
+import com.wm.app.b2b.server.invoke.InvokeChainProcessor;
+import com.wm.app.b2b.server.invoke.InvokeManager;
+import com.wm.app.b2b.server.invoke.ServiceStatus;
+import com.wm.data.IData;
+import com.wm.util.ServerException;
+import java.util.Iterator;
+
+/**
+ * An abstract base class for invoke chain processors.
+ */
+public abstract class AbstractInvokeChainProcessor implements InvokeChainProcessor {
+    /**
+     * Whether the processor is started or not.
+     */
+    protected volatile boolean started = false;
+
+    /**
+     * Processes a service invocation.
+     *
+     * @param iterator          Invocation chain.
+     * @param baseService       The invoked service.
+     * @param pipeline          The input pipeline for the service.
+     * @param serviceStatus     The status of the service invocation.
+     * @throws ServerException  If the service invocation fails.
+     */
+    @Override
+    public void process(Iterator iterator, BaseService baseService, IData pipeline, ServiceStatus serviceStatus) throws ServerException {
+        if (iterator.hasNext()) ((InvokeChainProcessor)iterator.next()).process(iterator, baseService, pipeline, serviceStatus);
+    }
+
+    /**
+     * Registers this class as an invocation handler and starts saving pipelines.
+     */
+    public synchronized void start() {
+        if (!started) {
+            started = true;
+            InvokeManager.getDefault().registerProcessor(this);
+        }
+    }
+
+    /**
+     * Unregisters this class as an invocation handler and stops saving pipelines.
+     */
+    public synchronized void stop() {
+        if (started) {
+            started = false;
+            InvokeManager.getDefault().unregisterProcessor(this);
+        }
+    }
+}
