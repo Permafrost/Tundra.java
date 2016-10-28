2efa0608dcfb3697ae98fc0605ba9d166b98dc58
Lachlan Dowding <lachlan@permafro.st>
2016-10-28 14:18:55 +1000

    Add new ServiceHelper methods for invoking, existence checking, and try/catch/finally pattern

--- src/main/java/permafrost/tundra/server/ServiceHelper.java
+++ src/main/java/permafrost/tundra/server/ServiceHelper.java
@@ -30,6 +30,7 @@ import com.wm.app.b2b.server.ServerAPI;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 import com.wm.app.b2b.server.ServiceSetupException;
+import com.wm.app.b2b.server.ServiceThread;
 import com.wm.app.b2b.server.ns.Namespace;
 import com.wm.data.IData;
 import com.wm.data.IDataCursor;
@@ -39,12 +40,16 @@ import com.wm.lang.ns.NSName;
 import com.wm.lang.ns.NSService;
 import com.wm.lang.ns.NSServiceType;
 import com.wm.net.HttpHeader;
+import permafrost.tundra.data.IDataHelper;
 import permafrost.tundra.data.IDataMap;
 import permafrost.tundra.lang.BytesHelper;
 import permafrost.tundra.lang.ExceptionHelper;
 import permafrost.tundra.lang.StringHelper;
+import permafrost.tundra.math.NormalDistributionEstimator;
 import permafrost.tundra.mime.MIMETypeHelper;
 import permafrost.tundra.net.http.HTTPHelper;
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -52,8 +57,6 @@ import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import javax.activation.MimeType;
-import javax.activation.MimeTypeParseException;
 
 /**
  * A collection of convenience methods for working with webMethods Integration Server services.
@@ -65,16 +68,24 @@ public final class ServiceHelper {
     private ServiceHelper() {}
 
     /**
-     * Returns the current call stack.
+     * Returns the call stack for the current invocation.
      *
-     * @return The current call stack.
+     * @return The call stack for the current invocation.
      */
     @SuppressWarnings("unchecked")
     public static List<NSService> getCallStack() {
         List<NSService> stack = (List<NSService>)InvokeState.getCurrentState().getCallStack();
         if (stack == null) stack = Collections.emptyList();
-
         return stack;
+    }
+
+    /**
+     * Returns true if the calling service is the top-level initiating service of the current thread.
+     *
+     * @return True if the calling service is the top-level initiating service of the current thread.
+     */
+    public static boolean isInitiator() {
+        return getCallStack().size() <= 1;
     }
 
     /**
@@ -243,9 +254,7 @@ public final class ServiceHelper {
      * @param headers  An IData document containing key value pairs to be set as headers in the given response.
      */
     private static void setHeaders(HttpHeader response, IData headers) {
-        IDataMap map = new IDataMap(headers);
-
-        for (Map.Entry<String, Object> entry : map) {
+        for (Map.Entry<String, Object> entry : IDataMap.of(headers)) {
             setHeader(response, entry.getKey(), entry.getValue());
         }
     }
@@ -262,5 +271,215 @@ public final class ServiceHelper {
             response.clearField(key);
             if (value != null) response.addField(key, value.toString());
         }
+    }
+
+    /**
+     * Returns true if the given service exists in Integration Server.
+     *
+     * @param service   The service to check existence of.
+     * @return          True if the given service exists in Integration Server.
+     */
+    public static boolean exists(String service) {
+        boolean exists = false;
+        try {
+            exists = exists(service, false);
+        } catch(ServiceException ex) {
+            // ignore exceptions
+        }
+        return exists;
+    }
+
+    /**
+     * Returns true if the given service exists in Integration Server.
+     *
+     * @param service           The service to check existence of.
+     * @param raise             If true and the service does not exist, an exception will be thrown.
+     * @return                  True if the given service exists in Integration Server.
+     * @throws ServiceException If raise is true and the given service does not exist.
+     */
+    public static boolean exists(String service, boolean raise) throws ServiceException {
+        boolean exists = NodeHelper.exists(service) && "service".equals(NodeHelper.getNodeType(service).toString());
+        if (raise && !exists) ExceptionHelper.raise("Service does not exist: " + service);
+        return exists;
+    }
+
+    /**
+     * Invokes the given service with the given pipeline synchronously.
+     *
+     * @param service           The service to be invoked.
+     * @param pipeline          The input pipeline used when invoking the service.
+     * @return                  The output pipeline returned by the service invocation.
+     * @throws ServiceException If the service throws an exception while being invoked.
+     */
+    public static IData invoke(String service, IData pipeline) throws ServiceException {
+        if (service == null) return null;
+
+        IData scope;
+        if (pipeline == null) {
+            scope = pipeline = IDataFactory.create();
+        } else {
+            scope = IDataUtil.clone(pipeline);
+        }
+
+        try {
+            scope = Service.doInvoke(NSName.create(service), scope);
+        } catch (Exception ex) {
+            ExceptionHelper.raise(ex);
+        }
+
+        IDataUtil.merge(scope, pipeline);
+
+        return pipeline;
+    }
+
+    /**
+     * Invokes the given service with the given pipeline asynchronously (in another thread).
+     *
+     * @param service           The service to be invoked.
+     * @param pipeline          The input pipeline used when invoking the service.
+     * @return                  The thread on which the service is being invoked.
+     */
+    public static ServiceThread thread(String service, IData pipeline) {
+        if (service == null) return null;
+
+        IData scope;
+        if (pipeline == null) {
+            scope = IDataFactory.create();
+        } else {
+            scope = IDataUtil.clone(pipeline);
+        }
+
+        return Service.doThreadInvoke(NSName.create(service), scope);
+    }
+
+    /**
+     * Waits for an asynchronously invoked service to complete.
+     *
+     * @param serviceThread     The service thread to wait on to finish.
+     * @return                  The output pipeline from the service invocation executed by the given thread.
+     * @throws ServiceException If an error occurs when waiting on the thread to finish.
+     */
+    public static IData join(ServiceThread serviceThread) throws ServiceException {
+        IData pipeline = null;
+
+        if (serviceThread != null) {
+            try {
+                pipeline = serviceThread.getIData();
+            } catch (Exception ex) {
+                ExceptionHelper.raise(ex);
+            }
+        }
+
+        return pipeline;
+    }
+
+    /**
+     * Invokes the given service a given number of times, and returns execution duration statistics. Exceptions thrown
+     * by the service are ignored / suppressed.
+     *
+     * @param service   The service to benchmark.
+     * @param pipeline  The input pipeline used when invoking the service.
+     * @param count     The sample count, or in other words the number of times to invoke the service.
+     * @return          Execution duration statistics generated by benchmarking the given service.
+     */
+    public static NormalDistributionEstimator benchmark(String service, IData pipeline, int count) {
+        NormalDistributionEstimator estimator = null;
+
+        try {
+            estimator = benchmark(service, pipeline, count, false);
+        } catch(ServiceException ex) {
+            // ignore exceptions
+        }
+
+        return estimator;
+    }
+
+    /**
+     * Invokes the given service a given number of times, and returns execution duration statistics.
+     *
+     * @param service           The service to benchmark.
+     * @param pipeline          The input pipeline used when invoking the service.
+     * @param count             The sample count, or in other words the number of times to invoke the service.
+     * @param raise             If true, exceptions thrown by the service will abort the benchmark.
+     * @return                  Execution duration statistics generated by benchmarking the given service.
+     * @throws ServiceException If the invoked service throws an exception and raise is true.
+     */
+    public static NormalDistributionEstimator benchmark(String service, IData pipeline, int count, boolean raise) throws ServiceException {
+        NormalDistributionEstimator estimator = new NormalDistributionEstimator("ms");
+
+        ServiceHelper.exists(service, true);
+
+        for (int i = 0; i < count; i++) {
+            long start = System.currentTimeMillis();
+            try {
+                invoke(service, pipeline);
+            } catch (ServiceException ex) {
+                if (raise) ExceptionHelper.raise(ex);
+            } finally {
+                long end = System.currentTimeMillis();
+                estimator.add(end - start);
+            }
+        }
+
+        return estimator;
+    }
+
+    /**
+     * Provides a try/catch/finally pattern for flow services.
+     *
+     * @param service           The service to be executed in the try clause of the try/catch/finally pattern.
+     * @param pipeline          The input pipeline used when invoking the services.
+     * @param catchService      The service to be executed in the catch clause of the try/catch/finally pattern.
+     * @param finallyService    The service to be executed in the finally clause of the try/catch/finally pattern.
+     * @return                  The output pipeline containing the results of the try/catch/finally pattern.
+     * @throws ServiceException If the service throws an exception while being invoked, and either no catch service is
+     *                          specified, or the catch service rethrows the exception.
+     */
+    public static IData ensure(String service, IData pipeline, String catchService, String finallyService) throws ServiceException {
+        try {
+            pipeline = invoke(service, pipeline);
+        } catch (Throwable exception) {
+            IDataCursor cursor = pipeline.getCursor();
+            IDataUtil.put(cursor, "$exception", exception);
+            IDataUtil.put(cursor, "$exception?", "true");
+            IDataUtil.put(cursor, "$exception.class", exception.getClass().getName());
+            IDataUtil.put(cursor, "$exception.message", exception.getMessage());
+
+            InvokeState invokeState = InvokeState.getCurrentState();
+            if (invokeState != null) {
+                IData exceptionInfo = IDataHelper.duplicate(invokeState.getErrorInfoFormatted(), true);
+                if (exceptionInfo != null) {
+                    IDataCursor ec = exceptionInfo.getCursor();
+                    String exceptionService = IDataUtil.getString(ec, "service");
+                    if (exceptionService != null) {
+                        IDataUtil.put(cursor, "$exception.service", exceptionService);
+                        BaseService baseService = Namespace.getService(NSName.create(exceptionService));
+                        if (baseService != null) {
+                            String packageName = baseService.getPackageName();
+                            if (packageName != null) {
+                                IDataUtil.put(ec, "package", packageName);
+                                IDataUtil.put(cursor, "$exception.package", packageName);
+                            }
+                        }
+                    }
+                    ec.destroy();
+                    IDataUtil.put(cursor, "$exception.info", exceptionInfo);
+                }
+            }
+
+            IDataUtil.put(cursor, "$exception.stack", ExceptionHelper.getStackTrace(exception));
+
+            cursor.destroy();
+
+            if (catchService == null) {
+                ExceptionHelper.raise(exception);
+            } else {
+                pipeline = invoke(catchService, pipeline);
+            }
+        } finally {
+            if (finallyService != null) pipeline = invoke(finallyService, pipeline);
+        }
+
+        return pipeline;
     }
 }
