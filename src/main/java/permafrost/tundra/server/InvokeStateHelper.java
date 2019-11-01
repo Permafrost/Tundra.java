e086c1bd7ec5cbee287bffa6edc81306021f4c46
Lachlan Dowding <lachlan@permafrost.space>
2019-11-01 11:09:45 +1000

    Add InvokeStateHelper methods for returning transport information

--- src/main/java/permafrost/tundra/server/InvokeStateHelper.java
+++ src/main/java/permafrost/tundra/server/InvokeStateHelper.java
@@ -25,6 +25,11 @@
 package permafrost.tundra.server;
 
 import com.wm.app.b2b.server.InvokeState;
+import com.wm.app.b2b.server.ProtocolInfoIf;
+import com.wm.data.IData;
+import com.wm.data.IDataCursor;
+import permafrost.tundra.data.CaseInsensitiveIData;
+import permafrost.tundra.data.IDataHelper;
 
 /**
  * A collection of convenience methods for working with InvokeState objects.
@@ -46,4 +51,125 @@ public final class InvokeStateHelper {
         if (invokeState == null) throw new NullPointerException("invokeState must not be null");
         return (InvokeState)invokeState.clone();
     }
+
+    /**
+     * Returns the current invoke state.
+     *
+     * @return The current invoke state.
+     */
+    public static InvokeState current() {
+        return InvokeState.getCurrentState();
+    }
+
+    /**
+     * Returns the transport information associated with the current invoke state.
+     *
+     * @return The transport information associated with the current invoke state.
+     */
+    public static IData currentTransport() {
+        return getTransport(current());
+    }
+
+    /**
+     * Returns the redacted transport information associated with the current invoke state.
+     *
+     * @return The redacted transport information associated with the current invoke state.
+     */
+    public static IData currentRedactedTransport() {
+        return getRedactedTransport(current());
+    }
+
+    /**
+     * Returns the transport information associated with the given invoke state.
+     *
+     * @param invokeState   The invoke state to retrieve the transport information from.
+     * @return              The transport information associated with the given invoke state.
+     */
+    public static IData getTransport(InvokeState invokeState) {
+        IData transport = null;
+        if (invokeState != null) {
+            ProtocolInfoIf protocolInfo = invokeState.getProtocolInfoIf();
+            if (protocolInfo != null) {
+                transport = (IData)protocolInfo.getProtocolProperty("transport");
+            }
+        }
+        return transport;
+    }
+
+    /**
+     * Returns the transport information associated with the given invoke state, with credentials redacted if
+     * applicable.
+     *
+     * @param invokeState   The invoke state to retrieve the transport information from.
+     * @return              The transport information associated with the given invoke state.
+     */
+    public static IData getRedactedTransport(InvokeState invokeState) {
+        IData transport = null;
+        if (invokeState != null) {
+            ProtocolInfoIf protocolInfo = invokeState.getProtocolInfoIf();
+            if (protocolInfo != null) {
+                transport = (IData)protocolInfo.getProtocolProperty("transport");
+
+                if (transport != null) {
+                    // take a copy of the transport data structure, so the following changes won't affect the source
+                    transport = IDataHelper.duplicate(transport, true);
+
+                    IDataCursor cursor = transport.getCursor();
+
+                    try {
+                        IData email = IDataHelper.get(cursor, "email", IData.class);
+                        if (email != null) {
+                            IDataCursor emailCursor = email.getCursor();
+                            try {
+                                // remove email content from transport, as it could be very large
+                                IDataHelper.remove(emailCursor, "content");
+                            } finally {
+                                emailCursor.destroy();
+                            }
+                        }
+
+                        IData http = IDataHelper.get(cursor, "http", IData.class);
+                        if (http != null) {
+                            IDataCursor httpCursor = http.getCursor();
+                            try {
+                                // redact authentication related headers, as they are confidential
+                                final String headersKey = "requestHdrs";
+                                IData headers = IDataHelper.get(httpCursor, headersKey, IData.class);
+                                IDataHelper.put(httpCursor, headersKey, redactHeaders(IDataHelper.sort(headers)), false);
+                            } finally {
+                                httpCursor.destroy();
+                            }
+                        }
+                    } finally {
+                        cursor.destroy();
+                    }
+                }
+            }
+        }
+        return transport;
+    }
+
+    /**
+     * Redacts the authentication related headers, if any, in the given set of HTTP headers.
+     *
+     * @param headers   The HTTP headers to redact.
+     * @return          The redacted HTTP headers.
+     */
+    private static IData redactHeaders(IData headers) {
+        if (headers != null) {
+            headers = CaseInsensitiveIData.of(headers);
+            IDataCursor headersCursor = headers.getCursor();
+            try {
+                String authorization = IDataHelper.get(headersCursor, "Authorization", String.class);
+                if (authorization != null && !authorization.equals("")) {
+                    IDataHelper.put(headersCursor, "Authorization", "REDACTED");
+                }
+            } finally {
+                headersCursor.destroy();
+            }
+        }
+
+        return headers;
+    }
+
 }
