e2ab2c62426f0c33d60adf6ad84d0a1feb4ba36a
Lachlan Dowding <lachlan@permafrost.space>
2020-03-16 17:59:36 +1000

    Fix PipelineHelper.validate and .sanitize to not throw NullPointerExceptions when there is no signature or fields

--- src/main/java/permafrost/tundra/flow/PipelineHelper.java
+++ src/main/java/permafrost/tundra/flow/PipelineHelper.java
@@ -30,6 +30,7 @@ import com.wm.data.IDataCursor;
 import com.wm.lang.ns.NSField;
 import com.wm.lang.ns.NSRecord;
 import com.wm.lang.ns.NSService;
+import com.wm.lang.ns.NSSignature;
 import com.wm.lang.schema.Validator;
 import com.wm.lang.xml.WMDocumentException;
 import permafrost.tundra.content.ValidationHelper;
@@ -37,6 +38,8 @@ import permafrost.tundra.content.ValidationResult;
 import permafrost.tundra.data.IDataHelper;
 import permafrost.tundra.lang.ExceptionHelper;
 import permafrost.tundra.server.ServiceHelper;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Convenience services for working with the invoke pipeline.
@@ -71,33 +74,36 @@ public class PipelineHelper {
 
         // we can only validate the pipeline when not debugging
         if (service != null && !"wm.server.flow:stepFlow".equals(service.getNSName().getFullName())) {
-            NSRecord record;
-            if (direction == InputOutputSignature.INPUT) {
-                record = service.getSignature().getInput();
-            } else {
-                record = service.getSignature().getOutput();
-            }
+            NSSignature signature = service.getSignature();
+            if (signature != null) {
+                NSRecord record;
+                if (direction == InputOutputSignature.INPUT) {
+                    record = signature.getInput();
+                } else {
+                    record = signature.getOutput();
+                }
 
-            if (record != null) {
-                Validator validator = Validator.create(pipeline, record, Validator.getDefaultOptions());
-                validator.setLocale(InvokeState.getCurrentLocale());
-                validator.setMaximumErrors(-1); // return all errors
+                if (record != null) {
+                    Validator validator = Validator.create(pipeline, record, Validator.getDefaultOptions());
+                    validator.setLocale(InvokeState.getCurrentLocale());
+                    validator.setMaximumErrors(-1); // return all errors
 
-                IDataCursor cursor = null;
-                try {
-                    IData scope = validator.validate();
-                    cursor = scope.getCursor();
+                    IDataCursor cursor = null;
+                    try {
+                        IData scope = validator.validate();
+                        cursor = scope.getCursor();
 
-                    boolean isValid = IDataHelper.getOrDefault(cursor, "isValid", Boolean.class, true);
+                        boolean isValid = IDataHelper.getOrDefault(cursor, "isValid", Boolean.class, true);
 
-                    if (!isValid) {
-                        IData[] errors = IDataHelper.get(cursor, "errors", IData[].class);
-                        result = ValidationHelper.buildResult(direction, isValid, errors, pipeline);
+                        if (!isValid) {
+                            IData[] errors = IDataHelper.get(cursor, "errors", IData[].class);
+                            result = ValidationHelper.buildResult(direction, isValid, errors, pipeline);
+                        }
+                    } catch (WMDocumentException ex) {
+                        ExceptionHelper.raiseUnchecked(ex);
+                    } finally {
+                        if (cursor != null) cursor.destroy();
                     }
-                } catch (WMDocumentException ex) {
-                    ExceptionHelper.raiseUnchecked(ex);
-                } finally {
-                    if (cursor != null) cursor.destroy();
                 }
             }
         }
@@ -137,12 +143,13 @@ public class PipelineHelper {
             if (record != null) {
                 NSField[] fields = record.getFields();
                 if (fields != null) {
-                    String[] names = new String[fields.length];
+                    List<String> names = new ArrayList<String>(fields.length);
                     for (int i = 0; i < fields.length; i++) {
-                        names[i] = fields[i].getName();
+                        if (fields[i] != null) {
+                            names.add(fields[i].getName());
+                        }
                     }
-
-                    IDataHelper.clear(pipeline, names);
+                    IDataHelper.clear(pipeline, names.toArray(new String[0]));
                 }
             }
         }
