f9c71b1a9c76ac3dbd8715e99507944d9fe50bd5
Lachlan Dowding <lachlan@permafrost.space>
2020-03-19 06:01:36 +1000

    Change IDataHTMLParser class to support portrait or landscape orientation for IData documents when serialized to an HTML table, and to support either returning an HTML fragment or complete HTML document

--- src/main/java/permafrost/tundra/data/IDataHTMLParser.java
+++ src/main/java/permafrost/tundra/data/IDataHTMLParser.java
@@ -39,16 +39,42 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * Serializes IData objects to HTML.
  */
 public class IDataHTMLParser extends IDataParser {
+    boolean fragment, portraitOrientation;
+
     /**
-     * Default constructor.
+     * Constructs a new IDataHTMLParser object which emits HTML fragments for embedding in HTML documents.
      */
     public IDataHTMLParser() {
+        this(true);
+    }
+
+    /**
+     * Constructs a new IDataHTMLParser object.
+     *
+     * @param fragment  IF true, emits HTML fragments for embedding in HTML documents, otherwise emits a complete HTML
+     *                  document.
+     */
+    public IDataHTMLParser(boolean fragment) {
+        this(fragment, true);
+    }
+
+    /**
+     * Constructs a new IDataHTMLParser object.
+     *
+     * @param fragment              IF true, emits HTML fragments for embedding in HTML documents, otherwise emits a
+     *                              complete HTML document.
+     * @param portraitOrientation   If true, IData documents will be serialized vertically with one key value per row.
+     */
+    public IDataHTMLParser(boolean fragment, boolean portraitOrientation) {
         super("text/html");
+        this.fragment = fragment;
+        this.portraitOrientation = portraitOrientation;
     }
 
     /**
@@ -89,7 +115,7 @@ public class IDataHTMLParser extends IDataParser {
     public String emit(IData input, int maxDepth) throws IOException {
         if (input == null) return HTMLEntity.NULL.toString();
         if (maxDepth < 1) maxDepth = Integer.MAX_VALUE;
-        return emit(new StringBuilder(), input, maxDepth, 0).toString();
+        return emit(new StringBuilder(), input, maxDepth, 0, portraitOrientation).toString();
     }
 
     /**
@@ -103,34 +129,42 @@ public class IDataHTMLParser extends IDataParser {
     public String emit(IData[] input, int maxDepth) throws IOException {
         if (input == null) return HTMLEntity.NULL.toString();
         if (maxDepth < 1) maxDepth = Integer.MAX_VALUE;
-        return emit(new StringBuilder(), input, maxDepth, 0).toString();
+        return emit(new StringBuilder(), input, maxDepth, 0, portraitOrientation).toString();
     }
 
     /**
      * Returns an HTML representation of the given IData object.
      *
-     * @param appendable        The Appendable to append the HTML to.
-     * @param input             The IData to convert to HTML.
-     * @param maxDepth          The maximum depth children will be encoded to.
-     * @param currentDepth      The current depth being encoded.
-     * @return                  The given Appendable for method chaining convenience.
-     * @throws IOException      If an IO error occurs.
+     * @param appendable            The Appendable to append the HTML to.
+     * @param input                 The IData to convert to HTML.
+     * @param maxDepth              The maximum depth children will be encoded to.
+     * @param currentDepth          The current depth being encoded.
+     * @param portraitOrientation   If true, IData documents will be serialized vertically with one key value per row.
+     * @return                      The given Appendable for method chaining convenience.
+     * @throws IOException          If an IO error occurs.
      */
-    protected Appendable emit(Appendable appendable, IData input, int maxDepth, int currentDepth) throws IOException {
+    @SuppressWarnings("deprecation")
+    protected Appendable emit(Appendable appendable, IData input, int maxDepth, int currentDepth, boolean portraitOrientation) throws IOException {
         input = IDataHelper.normalize(input);
         int size = IDataHelper.size(input);
+
+        if (currentDepth == 0 && !fragment) {
+            appendable.append("<html><head><style>")
+                      .append("body{margin:.5em;font:400 28pt/36pt Helvetica,Arial,sans-serif;} table{border-collapse:collapse;margin:0 !important;} th,td{padding:.5em;text-align:left;vertical-align:text-top;border:.375em solid #f0f0f0;font-weight:normal;} th{font-weight:bold;} thead>tr>th{background-color:#f9f9f9;border-color:#f0f0f0;} td{word-wrap:break-word;word-break:break-all;overflow-wrap:break-word;overflow:hidden;} td th{word-wrap:normal;word-break:normal;overflow-wrap:normal;overflow:visible;}")
+                      .append("</style></head><body>");
+        }
 
         if (size == 0) {
             appendable.append(HTMLEntity.EMPTY.toString());
         } else if (currentDepth >= maxDepth) {
             appendable.append(HTMLEntity.HORIZONTAL_ELLIPSIS.toString());
-        } else {
+        } else if (portraitOrientation) {
             IDataCursor cursor = input.getCursor();
             IData[] array = IDataUtil.getIDataArray(cursor, "recordWithNoID");
             cursor.destroy();
 
             if (array != null) {
-                emit(appendable, array, maxDepth, currentDepth);
+                emit(appendable, array, maxDepth, currentDepth, portraitOrientation);
             } else {
                 // table
                 appendable.append("<table class=\"IData\">");
@@ -162,13 +196,13 @@ public class IDataHTMLParser extends IDataParser {
                         appendable.append(HTMLEntity.NULL.toString());
                     } else {
                         if (value instanceof IData[] || value instanceof Table || value instanceof IDataCodable[] || value instanceof IDataPortable[] || value instanceof ValuesCodable[]) {
-                            emit(appendable, IDataHelper.toIDataArray(value), maxDepth, currentDepth + 1);
+                            emit(appendable, IDataHelper.toIDataArray(value), maxDepth, currentDepth + 1, portraitOrientation);
                         } else if (value instanceof IData || value instanceof IDataCodable || value instanceof IDataPortable || value instanceof ValuesCodable) {
-                            emit(appendable, IDataHelper.toIData(value), maxDepth, currentDepth + 1);
+                            emit(appendable, IDataHelper.toIData(value), maxDepth, currentDepth + 1, portraitOrientation);
                         } else if (value instanceof Object[][]) {
                             emit(appendable, (Object[][])value);
                         } else if (value instanceof Object[]) {
-                            emit(appendable, (Object[])value);
+                            emit(appendable, (Object[])value, portraitOrientation);
                         } else {
                             appendable.append(HTMLHelper.encode(value.toString()));
                         }
@@ -182,6 +216,71 @@ public class IDataHTMLParser extends IDataParser {
                 appendable.append("</tbody>");
                 appendable.append("</table>");
             }
+        } else {
+            IDataCursor cursor = input.getCursor();
+            IData[] array = IDataUtil.getIDataArray(cursor, "recordWithNoID");
+            cursor.destroy();
+
+            if (array != null) {
+                emit(appendable, array, maxDepth, currentDepth, portraitOrientation);
+            } else {
+                // table
+                appendable.append("<table class=\"IData\">");
+                // thead
+                appendable.append("<thead class=\"IData-Head\">");
+                appendable.append("<tr>");
+
+                List<String> keys = IDataHelper.getKeyList(input);
+                for (String key : keys) {
+                    appendable.append("<th>");
+                    appendable.append(HTMLHelper.encode(key));
+                    appendable.append("</th>");
+                }
+
+                appendable.append("</tr>");
+                appendable.append("</thead>");
+
+                // tbody
+                appendable.append("<tbody class=\"IData-Body\">");
+                appendable.append("<tr>");
+
+                cursor = input.getCursor();
+
+                try {
+                    for (String key : keys) {
+                        appendable.append("<td>");
+
+                        Object value = IDataHelper.get(cursor, key);
+
+                        if (value == null) {
+                            appendable.append(HTMLEntity.NULL.toString());
+                        } else {
+                            if (value instanceof IData[] || value instanceof Table || value instanceof IDataCodable[] || value instanceof IDataPortable[] || value instanceof ValuesCodable[]) {
+                                emit(appendable, IDataHelper.toIDataArray(value), maxDepth, currentDepth + 1, portraitOrientation);
+                            } else if (value instanceof IData || value instanceof IDataCodable || value instanceof IDataPortable || value instanceof ValuesCodable) {
+                                emit(appendable, IDataHelper.toIData(value), maxDepth, currentDepth + 1, portraitOrientation);
+                            } else if (value instanceof Object[][]) {
+                                emit(appendable, (Object[][]) value);
+                            } else if (value instanceof Object[]) {
+                                emit(appendable, (Object[]) value, portraitOrientation);
+                            } else {
+                                appendable.append(HTMLHelper.encode(value.toString()));
+                            }
+                        }
+                        appendable.append("</td>");
+                    }
+
+                    appendable.append("</tr>");
+                    appendable.append("</tbody>");
+                    appendable.append("</table>");
+                } finally {
+                    cursor.destroy();
+                }
+            }
+        }
+
+        if (currentDepth == 0 && !fragment) {
+            appendable.append("</body></html>");
         }
 
         return appendable;
@@ -190,24 +289,24 @@ public class IDataHTMLParser extends IDataParser {
     /**
      * Converts an IData[] to an HTML string.
      *
-     * @param appendable        The Appendable to append the HTML to.
-     * @param input             The IData[] to be converted.
-     * @param maxDepth          The maximum depth children will be encoded to.
-     * @param currentDepth      The current depth being encoded.
-     * @return                  The given Appendable for method chaining convenience.
-     * @throws IOException      If an IO error occurs.
+     * @param appendable            The Appendable to append the HTML to.
+     * @param input                 The IData[] to be converted.
+     * @param maxDepth              The maximum depth children will be encoded to.
+     * @param currentDepth          The current depth being encoded.
+     * @param portraitOrientation   If true, IData documents will be serialized vertically with one key value per row.
+     * @return                      The given Appendable for method chaining convenience.
+     * @throws IOException          If an IO error occurs.
      */
-    protected Appendable emit(Appendable appendable, IData[] input, int maxDepth, int currentDepth) throws IOException {
+    @SuppressWarnings("deprecation")
+    protected Appendable emit(Appendable appendable, IData[] input, int maxDepth, int currentDepth, boolean portraitOrientation) throws IOException {
         if (input.length == 0) {
             appendable.append(HTMLEntity.EMPTY.toString());
         } else if (currentDepth >= maxDepth) {
             appendable.append(HTMLEntity.VERTICAL_ELLIPSIS.toString());
         } else {
             String[] keys = IDataHelper.getKeys(input);
-
             // table
             appendable.append("<table class=\"IDataArray\">");
-
             // thead
             appendable.append("<thead class=\"IDataArray-Head\">");
             appendable.append("<tr>");
@@ -218,7 +317,6 @@ public class IDataHTMLParser extends IDataParser {
             }
             appendable.append("</tr>");
             appendable.append("</thead>");
-
             // tbody
             appendable.append("<tbody class=\"IDataArray-Body\">");
             for (IData document : input) {
@@ -232,13 +330,13 @@ public class IDataHTMLParser extends IDataParser {
                             appendable.append(HTMLEntity.NULL.toString());
                         } else {
                             if (value instanceof IData[] || value instanceof Table || value instanceof IDataCodable[] || value instanceof IDataPortable[] || value instanceof ValuesCodable[]) {
-                                emit(appendable, IDataHelper.toIDataArray(value), maxDepth, currentDepth + 1);
+                                emit(appendable, IDataHelper.toIDataArray(value), maxDepth, currentDepth + 1, portraitOrientation);
                             } else if (value instanceof IData || value instanceof IDataCodable || value instanceof IDataPortable || value instanceof ValuesCodable) {
-                                emit(appendable, IDataHelper.toIData(value), maxDepth, currentDepth + 1);
+                                emit(appendable, IDataHelper.toIData(value), maxDepth, currentDepth + 1, portraitOrientation);
                             } else if (value instanceof Object[][]) {
                                 emit(appendable, (Object[][])value);
                             } else if (value instanceof Object[]) {
-                                emit(appendable, (Object[])value);
+                                emit(appendable, (Object[])value, portraitOrientation);
                             } else {
                                 appendable.append(HTMLHelper.encode(value.toString()));
                             }
@@ -289,27 +387,46 @@ public class IDataHTMLParser extends IDataParser {
     /**
      * Converts an Object[] to an HTML string.
      *
-     * @param appendable        The Appendable to append the HTML to.
-     * @param input             The Object[] to be converted.
-     * @return                  The given Appendable for method chaining convenience.
-     * @throws IOException      If an IO error occurs.
+     * @param appendable            The Appendable to append the HTML to.
+     * @param input                 The Object[] to be converted.
+     * @param portraitOrientation   If true, IData documents will be serialized vertically with one key value per row.
+     * @return                      The given Appendable for method chaining convenience.
+     * @throws IOException          If an IO error occurs.
      */
-    protected Appendable emit(Appendable appendable, Object[] input) throws IOException {
-        appendable.append("<table>");
-        appendable.append("<tbody>");
-        for (Object item : input) {
-            appendable.append("<tr>");
-            appendable.append("<td>");
-            if (item == null) {
-                appendable.append(HTMLEntity.NULL.toString());
-            } else {
-                appendable.append(HTMLHelper.encode(item.toString()));
+    protected Appendable emit(Appendable appendable, Object[] input, boolean portraitOrientation) throws IOException {
+        if (portraitOrientation) {
+            appendable.append("<table>");
+            appendable.append("<tbody>");
+            for (Object item : input) {
+                appendable.append("<tr>");
+                appendable.append("<td>");
+                if (item == null) {
+                    appendable.append(HTMLEntity.NULL.toString());
+                } else {
+                    appendable.append(HTMLHelper.encode(item.toString()));
+                }
+                appendable.append("</td>");
+                appendable.append("</tr>");
             }
-            appendable.append("</td>");
+            appendable.append("</tbody>");
+            appendable.append("</table>");
+        } else {
+            appendable.append("<table>");
+            appendable.append("<tbody>");
+            appendable.append("<tr>");
+            for (Object item : input) {
+                appendable.append("<td>");
+                if (item == null) {
+                    appendable.append(HTMLEntity.NULL.toString());
+                } else {
+                    appendable.append(HTMLHelper.encode(item.toString()));
+                }
+                appendable.append("</td>");
+            }
             appendable.append("</tr>");
+            appendable.append("</tbody>");
+            appendable.append("</table>");
         }
-        appendable.append("</tbody>");
-        appendable.append("</table>");
 
         return appendable;
     }
