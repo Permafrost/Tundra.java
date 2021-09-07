cc13dec2bfd2ce5d6523ede0e25f58840fbe70f3
Lachlan Dowding <lachlan@permafrost.space>
2021-09-08 05:36:03 +1000

    Add Padder class for padding keys and String values in IData and IData[] objects

---
+++ src/main/java/permafrost/tundra/data/transform/string/Padder.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Lachlan Dowding
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package permafrost.tundra.data.transform.string;
+
+import permafrost.tundra.data.transform.Transformer;
+import permafrost.tundra.data.transform.TransformerMode;
+import permafrost.tundra.lang.StringHelper;
+
+/**
+ * Pads strings with a given character to a required length.
+ */
+public class Padder extends Transformer<String, String> {
+    /**
+     * The character used when padding a string.
+     */
+    protected char character;
+    /**
+     * The desired length to pad strings to.
+     */
+    protected int length;
+
+    /**
+     * Creates a new Padder object.
+     *
+     * @param mode          The transformer mode to use.
+     * @param recurse       Whether to recursively transform child IData documents and IData[] document lists.
+     * @param length        The desired length to pad string to.
+     * @param character     The character used to pad strings.
+     */
+    public Padder(TransformerMode mode, boolean recurse, int length, char character) {
+        super(String.class, String.class, mode, recurse, true, true, true);
+        this.length = length;
+        this.character = character;
+    }
+
+    /**
+     * Transforms the given key.
+     *
+     * @param key   The key to be transformed.
+     * @param value The value associated with the key being transformed.
+     * @return      The transformed key.
+     */
+    @Override
+    protected String transformKey(String key, Object value) {
+        return StringHelper.pad(key, length, character);
+    }
+
+    /**
+     * Transforms the given value.
+     *
+     * @param key   The key associated with the value being transformed.
+     * @param value The value to be transformed.
+     * @return      The transformed value.
+     */
+    @Override
+    protected String transformValue(String key, String value) {
+        return StringHelper.pad(value, length, character);
+    }
+}
